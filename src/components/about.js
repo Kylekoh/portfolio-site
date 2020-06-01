0f37dcfd8e7a5e3998653229ebf3bfbf29e1941e
고영일 <kyle_koh@goyeong-il-ui-MacBook-Pro.local>
2020-06-01 11:19:26 +0900

    fix about section media query

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,7 @@ const About = () => {
                 </div>
                 <div className={aboutStyles.imgContainer}>
                     <img className={aboutStyles.profilePic} src="https://scontent-gmp1-1.xx.fbcdn.net/v/t1.0-1/c50.50.620.620a/s160x160/417555_411630715560331_645666905_n.jpg?_nc_cat=109&_nc_sid=dbb9e7&_nc_oc=AQmclovjRG8eIsUIETMnPla71sspejiG0A-Y76CsGyjx5kR9V9QJrHhXQA9HEDLapY4&_nc_ht=scontent-gmp1-1.xx&oh=419cfefecca4d699ee3195a9e2274782&oe=5EF141C4"></img>
-                    <p>I am an athlete at heart, with an entrepreneurial spirit, a knack for online security, and a passion for coding. Having returned from my year of traveling, I pursued my first development job to kick off my career. I was hired as a full stack developer by Shop Your Own Mortgage, where I still work today. Since beginning my career there, I have been given the opportunity to shift into cybersecurity for the company. I am currently working towards my Certified Ethical Hacker certification while working as a full stack developer and lead security specialist.</p>
+                    <p>I am an athlete at heart, with an entrepreneurial spirit, a knack for online security, and a passion for coding. Having returned from my year of traveling, I pursued my first development job to kick off my career. I was hired as a full stack developer by Shop Your Own Mortgage, where I still work today. Since beginning my career there, I have been given the opportunity to shift into cybersecurity for the company.</p>
                 </div>
             </div>
         </section>
--- src/components/header.js
+++ src/components/header.js
@@ -13,27 +13,29 @@ const Header = () => {
 
     return (
             <nav className={headerStyles.navWrap}>
-                <a href="#home" className={headerStyles.logo}>
-                    <FaKorvue/>
-                    <span>Kyle's Portfoilo</span>
-                </a>
-                <ul className={`${headerStyles.navMenu} ${menuToggle ? `${headerStyles.toggleShow}` : ''}`}>                
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#works">Works</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contact</a>
-                    </li>
-                </ul>
-                <a  onClick={handleToggle} className={headerStyles.toggleButton}> 
-                    <FaBars />
-                </a>
+                <div className={headerStyles.navContainer}>
+                    <a href="#home" className={headerStyles.logo}>
+                        <FaKorvue/>
+                        <span>Kyle's Portfoilo</span>
+                    </a>
+                    <ul className={`${headerStyles.navMenu} ${menuToggle ? `${headerStyles.toggleShow}` : ''}`}>                
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#works">Works</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                    <a  onClick={handleToggle} className={headerStyles.toggleButton}> 
+                        <FaBars />
+                    </a>
+                </div>
             </nav>
     )
 }
