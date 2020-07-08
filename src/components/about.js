3738dca88668531f1e4a929205d3b47df2bcc380
고영일 <ykoh728@gmail.com>
2020-07-08 13:52:08 +0900

    modify footer content

--- src/components/about.js
+++ src/components/about.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React from "react"
 
-import aboutStyles from './about.module.scss';
-
+import aboutStyles from "./about.module.scss"
 
 const About = () => {
-    return (
-        <section id="about" className={aboutStyles.aboutWrap}>
-            <div className={aboutStyles.aboutContainer}>
-                <div className={aboutStyles.textContainer}>
-                    <h1>ABOUT</h1>
-                </div>
-                <div className={aboutStyles.imgContainer}>
-                    <img className={aboutStyles.profilePic} src="https://scontent-gmp1-1.xx.fbcdn.net/v/t1.0-1/c50.50.620.620a/s160x160/417555_411630715560331_645666905_n.jpg?_nc_cat=109&_nc_sid=dbb9e7&_nc_oc=AQmclovjRG8eIsUIETMnPla71sspejiG0A-Y76CsGyjx5kR9V9QJrHhXQA9HEDLapY4&_nc_ht=scontent-gmp1-1.xx&oh=419cfefecca4d699ee3195a9e2274782&oe=5EF141C4"></img>
-                    <p>날씨가 너무나도 좋았다. 뭉게구름이 너무나 이쁘게 피어있었다.날씨가 너무나도 좋았다. 뭉게구름이 너무나 이쁘게 피어있었다.날씨가 너무나도 좋았다. 뭉게구름이 너무나 이쁘게 피어있었다.날씨가 너무나도 좋았다. 뭉게구름이 너무나 이쁘게 피어있었다.날씨가 너무나도 좋았다. 뭉게구름이 너무나 이쁘게 피어있었다.</p>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section id="about" className={aboutStyles.aboutWrap}>
+      <div className={aboutStyles.aboutContainer}>
+        <div className={aboutStyles.textContainer}>
+          <h1>ABOUT</h1>
+        </div>
+        <div className={aboutStyles.imgContainer}>
+          <img
+            className={aboutStyles.profilePic}
+            src={require("../images/mypicture.jpg")}
+          />
+          <p>
+            날씨가 너무나도 좋았다. 뭉게구름이 너무나 이쁘게 피어있었다.날씨가
+            너무나도 좋았다. 뭉게구름이 너무나 이쁘게 피어있었다.날씨가 너무나도
+            좋았다. 뭉게구름이 너무나 이쁘게 피어있었다.날씨가 너무나도 좋았다.
+            뭉게구름이 너무나 이쁘게 피어있었다.날씨가 너무나도 좋았다.
+            뭉게구름이 너무나 이쁘게 피어있었다.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default About;
+export default About
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,50 +1,70 @@
-import React from 'react'
+import React from "react"
 
-import {FaFacebookF, FaGithub, FaInstagramSquare, FaLinkedin} from 'react-icons/fa'
-import { FiMail } from "react-icons/fi";
+import {
+  FaFacebookF,
+  FaGithub,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaBlogger,
+} from "react-icons/fa"
+import { FiMail } from "react-icons/fi"
 
-import footerStyles from './footer.module.scss';
-
+import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
-    return (
-        <section id='contact' className={footerStyles.footerWrap}>
-            <p>저에 대해 더 알고싶다면?</p>
-            <div className={footerStyles.socialIcons}>
-                <a href="https://www.instagram.com/koztwe/" className={footerStyles.socialButtons}>
-                    <span className={footerStyles.socialIconItem}>
-                        <FaInstagramSquare />
-                    </span>
-                </a>
-                <a href="https://www.facebook.com/young.i.koh" className={footerStyles.socialButtons}>
-                    <span className={footerStyles.socialIconItem}>
-                        <FaFacebookF />
-                    </span>
-                </a>
-                <a href="https://github.com/Kylekoh" className={footerStyles.socialButtons}>
-                    <span className={footerStyles.socialIconItem}>
-                        <FaGithub />
-                    </span>
-                </a>
-                <a href="https://www.linkedin.com/in/kylekoh16/" className={footerStyles.socialButtons}>
-                    <span className={footerStyles.socialIconItem}>
-                        <FaLinkedin />
-                    </span>
-                </a>
-            </div>
-            <div className={footerStyles.mailWrap}>
-                <p className={footerStyles.mailContainer}>
-                    <FiMail />
-                    <span>ykoh728@gmail.com</span>
-                </p>
-            </div>
-            <div className={footerStyles.right}>
-                <span>
-                    &#169; 2020. All rights reserved.
-                </span>
-            </div>
-        </section>
-    )
+  return (
+    <section id="contact" className={footerStyles.footerWrap}>
+      <p>저에 대해 더 알고싶다면?</p>
+      <div className={footerStyles.socialIcons}>
+        <a
+          href="https://github.com/Kylekoh"
+          className={footerStyles.socialButtons}
+          target="_blank"
+        >
+          <span className={footerStyles.socialIconItem}>
+            <FaGithub />
+          </span>
+        </a>
+        <a
+          href="https://velog.io/@ko12ztwe"
+          className={footerStyles.socialButtons}
+          target="_blank"
+        >
+          <span className={footerStyles.socialIconItem}>
+            <FaBlogger />
+          </span>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/kylekoh16/"
+          className={footerStyles.socialButtons}
+          target="_blank"
+        >
+          <span className={footerStyles.socialIconItem}>
+            <FaLinkedin />
+          </span>
+        </a>
+        <a
+          href="https://www.instagram.com/koztwe/"
+          className={footerStyles.socialButtons}
+          target="_blank"
+        >
+          <span className={footerStyles.socialIconItem}>
+            <FaInstagramSquare />
+          </span>
+        </a>
+      </div>
+      <div className={footerStyles.mailWrap}>
+        <p className={footerStyles.mailContainer}>
+          <FiMail />
+          <span>ykoh728@gmail.com</span>
+        </p>
+      </div>
+      <div className={footerStyles.right}>
+        <span>&#169; 2020. All rights reserved.</span>
+      </div>
+    </section>
+  )
 }
 
 export default Footer
